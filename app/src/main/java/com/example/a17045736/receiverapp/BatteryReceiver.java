d6982b3deebe43dce64a5520293fc925e677cdd5
YAP YU QI <17045736@rp.edu.sg>
2018-08-15 15:15:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a17045736/receiverapp/BatteryReceiver.java
@@ -0,0 +1,21 @@
+package com.example.a17045736.receiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+
+public class BatteryReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast my_toast = Toast.makeText(context, "Low Battery", Toast.LENGTH_LONG);
+        my_toast.show();
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/a17045736/receiverapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a17045736.receiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    BroadcastReceiver br = new BatteryReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction("android.intent.action.BATTERY_LOW");
+        this.registerReceiver(br, filter);
+    }
+
+    protected void onDetroy(){
+        super.onDestroy();
+        this.unregisterReceiver(br);
+    }
+}
